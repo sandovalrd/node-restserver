26873e3641c71d012f52dfe07df0fee08eb7d0e2
Rafael Sandoval <sandovalrd@gmail.com>
2019-08-24 23:58:09 -0300

    variable tiempo de expiracion en el ambiente de produccion

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     seedToken: process.env.SEED_TOKEN,
-    cliend_id: process.env.CLIEND_ID
+    cliend_id: process.env.CLIEND_ID,
+    expiresIn: 60 * 60
 };
